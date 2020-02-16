056eca8fed16ce06d72b710e83a3fdded8a909d9
gnerikuser <57920717+gnerikuser@users.noreply.github.com>
2020-02-15 22:39:25 -0300

    Create next-version.php

---
+++ next-version.php
@@ -0,0 +1,10 @@
+<title>OGWP</title>
+<link rel="icon" href="logo.ico">
+<link rel="stylesheet" type="text/css" href="the.css">
+<link href="https://fonts.googleapis.com/css?family=Press+Start+2P|Roboto&display=swap" rel="stylesheet">
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+<p id="sorry">The next version is:</p>
+<p id="sorry"><?php include "next-version.txt"?></p>
